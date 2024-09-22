9741c7d8d3a8b19204a70ac35139f71fdddd05cf
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-09-22 11:28:29 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,21 @@
-
+from src.data_loader import download
+from src.preprocessing import import_data
 
 def main():
 
-  pass
+  # Download the data
+  download()
 
-if __name__ == '__main__':
+  # Process the data and return the dataframes
+  dst, sunspots, solar_wind = import_data()
+
+  # TODO: Consolidate the data
+  
+  # TODO: Split the data
+
+  # TODO: Train different models
+
+  # TODO: Compare the results of the different models
+
+if __name__== '__main__':
   main()
